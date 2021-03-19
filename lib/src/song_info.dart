22be9bef2dbca1b29149ae4326b368c458c5fc7b
Hieu <dqhieu01@gmail.com>
2021-03-19 21:36:59 +0700

    Make SongInfo class mutable

--- lib/src/song_info.dart
+++ lib/src/song_info.dart
@@ -2,65 +2,148 @@ part of flutter_audio_query;
 
 /// SongInfo class holds all information about a specific song audio file.
 class SongInfo extends DataModel {
-  SongInfo._(Map<dynamic, dynamic> map) : super._(map);
+  SongInfo._(Map<dynamic, dynamic> map) : super._(map) {
+    /// Returns the album id which this song appears.
+    albumId = map['album_id'];
+
+    /// Returns the artist id who create this audio file.
+    artistId = map['artist_id'];
+
+    /// Returns the artist name who create this audio file.
+    artist = map['artist'];
+
+    /// Returns the album title which this song appears.
+    album = map['album'];
+
+    // Returns the genre name which this song belongs.
+    //genre  = map['genre_name'];
+
+    /// Returns the song title.
+    title = map['title'];
+
+    /// Returns the song display name. Display name string
+    /// is a combination of [Track number] + [Song title] [File extension]
+    /// Something like 1 My pretty song.mp3
+    displayName = map['_display_name'];
+
+    /// Returns the composer name of this song.
+    composer = map['composer'];
+
+    /// Returns the year of this song was created.
+    year = map['year'];
+
+    /// Returns the album track number if this song has one.
+    track = map['track'];
+
+    /// Returns a String with a number in milliseconds (ms) that is the duration of this audio file.
+    duration = map['duration'];
+
+    /// Returns in ms, playback position when this song was stopped.
+    /// from the last time.
+    bookmark = map['bookmark'];
+
+    /// Returns a String with a file path to audio data file
+    filePath = map['_data'];
+
+    uri = map["uri"];
+
+    /// Returns a String with the size, in bytes, of this audio file.
+    fileSize = map['_size'];
+
+    ///Returns album artwork path which current song appears.
+    albumArtwork = map['album_artwork'];
+
+    isMusic = map['is_music'];
+
+    isPodcast = map['is_podcast'];
+
+    isRingtone = map['is_ringtone'];
+
+    isAlarm = map['is_alarm'];
+
+    isNotification = map['is_notification'];
+  }
+
+  SongInfo(
+      {this.albumId,
+      this.artistId,
+      this.artist,
+      this.album,
+      this.title,
+      this.displayName,
+      this.composer,
+      this.year,
+      this.track,
+      this.duration,
+      this.bookmark,
+      this.filePath,
+      this.uri,
+      this.fileSize,
+      this.albumArtwork,
+      this.isMusic,
+      this.isPodcast,
+      this.isRingtone,
+      this.isAlarm,
+      this.isNotification})
+      : super._({});
 
   /// Returns the album id which this song appears.
-  String get albumId => _data['album_id'];
+  String albumId;
 
   /// Returns the artist id who create this audio file.
-  String get artistId => _data['artist_id'];
+  String artistId;
 
   /// Returns the artist name who create this audio file.
-  String get artist => _data['artist'];
+  String artist;
 
   /// Returns the album title which this song appears.
-  String get album => _data['album'];
+  String album;
 
   // Returns the genre name which this song belongs.
-  //String get genre => _data['genre_name'];
+  //String genre ;
 
   /// Returns the song title.
-  String get title => _data['title'];
+  String title;
 
   /// Returns the song display name. Display name string
   /// is a combination of [Track number] + [Song title] [File extension]
   /// Something like 1 My pretty song.mp3
-  String get displayName => _data['_display_name'];
+  String displayName;
 
   /// Returns the composer name of this song.
-  String get composer => _data['composer'];
+  String composer;
 
   /// Returns the year of this song was created.
-  String get year => _data['year'];
+  String year;
 
   /// Returns the album track number if this song has one.
-  String get track => _data['track'];
+  String track;
 
   /// Returns a String with a number in milliseconds (ms) that is the duration of this audio file.
-  String get duration => _data['duration'];
+  String duration;
 
   /// Returns in ms, playback position when this song was stopped.
   /// from the last time.
-  String get bookmark => _data['bookmark'];
+  String bookmark;
 
   /// Returns a String with a file path to audio data file
-  String get filePath => _data['_data'];
+  String filePath;
 
-  String get uri => _data["uri"];
+  String uri;
 
   /// Returns a String with the size, in bytes, of this audio file.
-  String get fileSize => _data['_size'];
+  String fileSize;
 
   ///Returns album artwork path which current song appears.
-  String get albumArtwork => _data['album_artwork'];
+  String albumArtwork;
 
-  bool get isMusic => _data['is_music'];
+  bool isMusic;
 
-  bool get isPodcast => _data['is_podcast'];
+  bool isPodcast;
 
-  bool get isRingtone => _data['is_ringtone'];
+  bool isRingtone;
 
-  bool get isAlarm => _data['is_alarm'];
+  bool isAlarm;
 
-  bool get isNotification => _data['is_notification'];
+  bool isNotification;
 }
